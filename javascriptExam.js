7dbe17c6b96763920db8ce5817b31245207ba726
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2019-05-16 19:50:11 -0400

    some examples

---
+++ javascriptExam.js
@@ -0,0 +1,66 @@
+// indexOf function() provide the index of the first accurace
+let myName = "Paula Sanchez"
+console.log(myName.indexOf("San"))
+// ===> 6
+
+
+//repeat. reapet the number as much time as we need
+console.log("#0".repeat(2))
+//===> #0#0
+
+//include case sensitive search boolean 
+console.log("Reilly".includes("ll"))
+//====> true
+
+
+
+//
+var words = [
+  'mystery',
+  'brother',
+  'aviator',
+  'crocodile',
+  'pearl',
+  'orcharditaaaas',
+  'crackpot'
+];
+
+
+// find the logest workd
+function findLongestWord(words){
+  var maxi="";
+  if(words && words.length ){
+	words.forEach(function(el,i){
+	   if (el.length === maxi.length){
+          maxi = el[i-1];
+     	}
+		else 
+		if(el.length+1 > maxi.length)
+			{
+				maxi = el;
+			//	console.log(maxi);
+		}
+			
+		});
+			console.log(maxi);
+		return maxi;
+  }
+  else {return undefined;}
+}
+findLongestWord(words);
+
+//.toUpperCase()
+//map use return and return an array of the same size
+let carArray=["honda", "mercedes", "lexus", "nissan"]
+
+carArray.map(car => {
+  return car.toUpperCase();
+})
+
+
+// splice(start, deletecount)
+//        start= start changing the array
+//        deletecount =
+
+let deletArray = ["cata", "pipe", "dani", "cristian"]
+console.log(deletArray.splice(2,3))

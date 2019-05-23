48d8ed98c2d73b89bdeac76242cbff95eda5ad5c
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2019-05-23 19:31:40 -0400

    highandlow

--- javascriptExam.js
+++ javascriptExam.js
@@ -76,3 +76,23 @@ console.log(deletArray.splice(2,3))
 let newArray = array.map(function(number){
   return number * 2;
 })
+
+
+
+//In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+//highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+function highAndLow(numbers){
+  let arrayNumber = numbers.split(" ");
+  // console.log(arrayNumber)
+  let newArray = arrayNumber.map( value =>{
+       return parseInt(value)
+  })
+  // ...
+ let maxNumber = Math.max(...newArray);
+  let minNumber =Math.min(...newArray);
+  let both =+  maxNumber.toString()+ " "+ minNumber.toString()
+  return both
+}

3a747c4413b263a0b77d4021d99f1be467739cbf
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2019-05-31 23:36:11 -0400

    conver the firs t letter uper case

--- javascriptExam.js
+++ javascriptExam.js
@@ -173,4 +173,27 @@ function GetSum(a,b){
         return true;
   }
    return false;
+
+
+
+  /*Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
+
+      Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+      Example:
+
+      Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+      Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+      Note that the Java version expects a return value of null for an empty string or null. */
+
+   String.prototype.toJadenCase = function (dato) {
+    let inf= dato.split(" ");
+    let lasdato=""
+    inf.forEach(function(elem,index){
+      lasdato += elem.charAt(0).toUpperCase() + elem.slice(1) + " ";
+    });
+    return lasdato;
+    };
+   
+   String.prototype.toJadenCase("How can mirrors be real if our eyes aren't real")
 }

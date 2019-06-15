e996993e5bfcba8be3d5b33c4c6960f62ea50b4e
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2019-06-15 13:44:48 -0400

    stack object

--- javascriptExam.js
+++ javascriptExam.js
@@ -256,3 +256,40 @@ Object.keys(obj)
      }
    }
   
+
+   //***  stack object   */
+//Create a stack
+   let Stack = function(){
+     this.count = 0;
+     this.storage ={};
+// add a value onto the end of the stack
+     this.push = function(value){
+       this.storage[this.count] = value;
+       this.count++;
+     }
+// Removes and return the value at the end of the stack
+    this.pop = function(){
+      if(this.count === 0){
+        return undefined;
+      }
+  this.count--;
+  let result = this.storage[this.count];
+  delete this.storage[this.count];
+  return result;
+  }
+    this.size = function(){
+      return this.count;
+    }
+//return the value at the end of the stack
+    this.peek=function(){
+      return this.storage[this.count-1];
+    }
+   }
+
+ let myStack = new Stack();
+ 
+ myStack.push(1);
+ myStack.push(2);
+ console.log(myStack.peek()); //2
+ console.log(myStack.pop());  //2
+ console.log(myStack.peek()); //1

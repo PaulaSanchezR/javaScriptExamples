294c49d24bc596b00959f01255591d667696c1bc
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2019-06-15 13:16:16 -0400

    lang version palidrom

--- javascriptExam.js
+++ javascriptExam.js
@@ -197,3 +197,62 @@ function GetSum(a,b){
    
    String.prototype.toJadenCase("How can mirrors be real if our eyes aren't real")
 }
+
+//************BEST SOLUTION */
+
+    String.prototype.toJadenCase = function () { 
+      return this.split(" ").map(function(word){
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }).join(" ");
+    }
+
+
+ /*findLastKey
+Returns the last key that satisfies the provided testing function. Otherwise undefined is returned.
+
+Use Object.keys(obj) to get all the properties of the object, Array.prototype.reverse() to reverse their order and Array.prototype.find() to test the provided function for each key-value pair. The callback receives three arguments - the value, the key and the object.
+
+ */   
+
+const findLastKey = (obj, fn) =>
+Object.keys(obj)
+  .reverse()
+  .find(key => fn(obj[key], key, obj));
+  findLastKey(
+    {
+      barney: { age: 36, active: true },
+      fred: { age: 40, active: false },
+      pebbles: { age: 1, active: true }
+    },
+    o => o['active']
+  ); // 'pebbles'
+
+
+
+  /**STACKS
+   * 
+   *  POLINDROME
+   */
+  
+   function checkPolindromeWord(word){
+     if (word.length == 0){
+       return console.log("no word")
+     }
+     const letters=[]; 
+     let rword="";
+     // adding workd to the array
+     for (let i=0; i< word.length; i++){
+          letters.push(word[i]);
+      }
+    // concat the work in reverse
+     for(let i=0;i< word.length; i++){
+          rword += letters.pop()
+     }
+     if(rword === word){
+       return console.log(word + "is a palindrom")
+     }
+     else {
+       return console.log(word + " is not a palidrome")
+     }
+   }
+  
